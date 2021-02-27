6f6aecfc9b3ae612c86d3191e7fcd6f7e7755d88
Aaron Wiens <aaron@awwebsolutions.com>
2021-02-27 08:17:13 -0600

    Added some data helpers for the tests.

---
+++ src/SmartFamily.Gedcom.Tests/DataHelperExtensions/GedcomIndividualRecordFactory.cs
@@ -0,0 +1,63 @@
+﻿using SmartFamily.Gedcom.Models;
+
+namespace SmartFamily.Gedcom.Tests.DataHelperExtensions
+{
+    /// <summary>
+    /// Helper functions to create people with names for unit testing.
+    /// </summary>
+    public static class GedcomIndividualRecordFactory
+    {
+        /// <summary>
+        /// Create an individual with a specific name.
+        /// </summary>
+        /// <param name="gedcomDb">The gedcom database to attach the name and individual to.</param>
+        /// <param name="name">The name to place directly into the name field.</param>
+        /// <returns>The constructed individual.</returns>
+        public static GedcomIndividualRecord NamedPerson(this GedcomDatabase gedcomDb, string name)
+        {
+            var personName = new GedcomName();
+            personName.Level = 1;
+            personName.Database = gedcomDb;
+            personName.Name = name;
+            personName.PreferredName = true;
+
+            var person = new GedcomIndividualRecord(gedcomDb);
+            person.Names.Clear();
+            person.Names.Add(personName);
+            return person;
+        }
+
+        /// <summary>
+        /// Create an individual with a specific name.
+        /// </summary>
+        /// <param name="gedcomDb">The gedcom database to attach the name and individual to.</param>
+        /// <param name="givenName">The given name (first name) to attach to the new individual.</param>
+        /// <param name="surname">The surname (last name) to attach to the new individual.</param>
+        /// <returns>The constructed individual.</returns>
+        public static GedcomIndividualRecord NamedPerson(this GedcomDatabase gedcomDb, string givenName, string surname)
+        {
+            var person = new GedcomIndividualRecord(gedcomDb);
+            person.Names.Clear();
+            if (!string.IsNullOrWhiteSpace(givenName) || !string.IsNullOrWhiteSpace(surname))
+            {
+                var personName = new GedcomName();
+                personName.Level = 1;
+                personName.Database = gedcomDb;
+                if (!string.IsNullOrWhiteSpace(givenName))
+                {
+                    personName.Given = givenName;
+                }
+
+                personName.PreferredName = true;
+                if (!string.IsNullOrWhiteSpace(surname))
+                {
+                    personName.Surname = surname;
+                }
+
+                person.Names.Add(personName);
+            }
+
+            return person;
+        }
+    }
+}
