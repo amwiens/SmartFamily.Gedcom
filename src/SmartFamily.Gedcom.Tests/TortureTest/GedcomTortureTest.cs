7c881e14541455672be71be0e41394928656bd41
Aaron Wiens <aaron@awwebsolutions.com>
2021-02-26 23:20:08 -0600

    Added torture test.

---
+++ src/SmartFamily.Gedcom.Tests/TortureTest/GedcomTortureTest.cs
@@ -0,0 +1,19 @@
+﻿using SmartFamily.Gedcom.Parser;
+
+using Xunit;
+
+namespace SmartFamily.Gedcom.Tests.TortureTest
+{
+    /// <summary>
+    /// Loads the torture test files to test every tag can be read at least without falling over.
+    /// </summary>
+    public class GedcomTortureTest
+    {
+        [Theory]
+        [InlineData(".\\Data\\TortureTEsts\\TGC551.ged")]
+        private void Files_can_be_loaded_without_exceptions(string sourceFile)
+        {
+            GedcomRecordReader.CreateReader(sourceFile);
+        }
+    }
+}
